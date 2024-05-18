8edde679462d7645ea89aafa410f77be04e1ebeb
Anne Hunt <acathunt@gmail.com>
2024-05-18 16:32:31 -0600

    forms created

---
+++ client/src/components/IngredientForm.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { computed, ref } from 'vue';
+import Pop from '../utils/Pop.js';
+import { logger } from '../utils/Logger.js';
+import { AppState } from '../AppState.js';
+import { ingredientService } from '../services/IngredientService.js';
+
+const ingredients = computed(()=> AppState.ingredients)
+
+const ingredient = ref({
+  quantity: '',
+  name: ''
+})
+
+async function createIngredient(){
+    try {
+      const ingredientData = ingredient
+      await ingredientService.createIngredients(ingredientData)
+      resetForm()
+    }
+    catch (error){
+      Pop.toast("Unable to submit ingredient", 'error');
+      logger.error("Unable to submit ingredient", error)
+    }
+}
+
+async function updateIngredient(ingredientId){
+  try {
+    const ingredientData = ingredient
+    await ingredientService.updateIngredient(ingredientData, ingredientId)
+    resetForm()
+  }
+  catch (error){
+    Pop.toast("Unable to update",'error');
+    logger.error("Unable to update ingredient", error)
+  }
+}
+
+async function trashIngredient(ingredientId){
+  try {
+    Pop.confirm("Do you really want to delete this ingredient?")
+    if(!confirm){
+      return
+    }
+    ingredientService.trashIngredient(ingredientId)
+    resetForm()
+  }
+  catch (error){
+    Pop.toast("Unable to delete", 'error');
+    logger.error("Unable to delete ingredient", error)
+  }
+}
+
+function resetForm(){
+    ingredient.value.quantity = ''
+    ingredient.value.name = ''
+}
+</script>
+
+
+<template>
+  <div v-if="ingredients">
+    <form v-for="ingredient in ingredients" :key="ingredient.id" @submit.prevent="updateIngredient(ingredient.id)">
+      <div class="input-group mb-3">
+        <input v-model="ingredient.quantity" type="text" class="form-control" :placeholder="ingredient.quantity" aria-label="Quantity">
+        <span class="input-group-text">OF</span>
+        <input v-model="ingredient.name" type="text" class="form-control" :placeholder="ingredient.name" aria-label="Name" maxlength="250" aria-describedby="ingredientButton" name="ingredient" id="ingredient">
+        <button class="btn btn-outline-primary" type="submit" id="ingredientButton"><i class="mdi mdi-pencil"></i></button>
+        <button class="btn btn-outline-danger" type="button" id="ingredientButton" @click="trashIngredient(ingredient.id)"><i class="mdi mdi-trash-can"></i></button>
+      </div>
+    </form>
+  </div>
+  <div>
+    <form @submit.prevent="createIngredient()">  
+      <div class="input-group mb-3">
+        <input v-model="ingredient.quantity" type="text" class="form-control" placeholder="Quantity" aria-label="Quantity">
+        <span class="input-group-text">OF</span>
+        <input v-model="ingredient.name" type="text" class="form-control" placeholder="Name" aria-label="Name" maxlength="250" aria-describedby="ingredientButton" name="ingredient" id="ingredient">
+        <button class="btn btn-outline-success" type="submit" id="ingredientButton"><i class="mdi mdi-plus"></i></button>
+      </div>
+    </form>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- client/src/components/RecipeForm.vue
+++ client/src/components/RecipeForm.vue
@@ -1,11 +1,14 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import Pop from '../utils/Pop.js';
 import { logger } from '../utils/Logger.js';
 import { recipeService } from '../services/RecipeService.js';
+import { AppState } from '../AppState.js';
+
+const activeRecipe = computed(()=> AppState.activeRecipe)
 
 const recipe = ref({
-    ingredient: [],
+    title: '',
     instructions: ''
 })
 
@@ -19,14 +22,43 @@ async function createRecipe(){
       logger.error("Unable to submit recipe", error)
     }
 }
+
+async function updateRecipe(recipeId){
+try {
+  const recipeData = recipe
+  await recipeService.updateRecipe(recipeData, recipeId)
+}
+catch (error){
+  Pop.toast("Unable to update recipe", 'error');
+  logger.error("Unable to update recipe", error)
+}
+}
 </script>
 
 
 <template>
-<form @submit.prevent="createRecipe()">
-    <label for="ingredient">Ingredients:</label>
-    <input v-modal="recipe.ingredient" type="text" name="ingredient" id="ingredient" maxlength="250">
-    <button type="submit">SUBMIT</button>
+<form v-if="activeRecipe == null" @submit.prevent="createRecipe()">
+  <div class="mb-3">
+    <label for="title" class="form-label">Recipe Title</label>
+    <input v-modal="recipe.title" type="text" class="form-control" id="titleInput" placeholder="">
+  </div>
+  <div class="mb-3">
+    <label for="instructions" class="form-label">Instructions</label>
+    <textarea v-modal="recipe.instructions" class="form-control" id="instructionsInput" rows="5"></textarea>
+    <button class="btn btn-outline-secondary" type="button" id="ingredientButton">Submit</button>
+  </div>
+</form>
+
+<form v-else @submit.prevent="updateRecipe()">
+  <div class="mb-3">
+    <label for="title" class="form-label">Recipe Title</label>
+    <input v-modal="recipe.title" type="text" class="form-control" id="titleInput" :placeholder="activeRecipe.title">
+  </div>
+  <div class="mb-3">
+    <label for="instructions" class="form-label">Instructions</label>
+    <textarea v-modal="recipe.instructions" class="form-control" id="instructionsInput" rows="5" :placeholder="activeRecipe.instructions"></textarea>
+    <button class="btn btn-outline-secondary" type="button" id="ingredientButton">Update</button>
+  </div>
 </form>
 </template>
 
