39931dfb53002ee9e2c214023371c7c10b7d57c0
Anne Hunt <acathunt@gmail.com>
2024-05-19 14:28:05 -0600

    working on favorites showing up

--- client/src/services/AccountService.js
+++ client/src/services/AccountService.js
@@ -1,5 +1,6 @@
 import { AppState } from '../AppState'
 import { Account } from '../models/Account.js'
+import { Favorite } from '../models/Favorite.js'
 import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
 
@@ -8,10 +9,18 @@ class AccountService {
     try {
       const res = await api.get('/account')
       AppState.account = new Account(res.data)
+      this.getFavorites()
     } catch (err) {
       logger.error('HAVE YOU STARTED YOUR SERVER YET???', err)
     }
   }
+
+  async getFavorites() {
+    const response = await api.get("/account/favorites")
+    const favorites = response.data.map(favoriteData => new Favorite(favoriteData))
+    AppState.favorites = favorites
+    logger.log("got favorites", favorites)  
+}
 }
 
 export const accountService = new AccountService()
--- client/src/services/FavoriteService.js
+++ client/src/services/FavoriteService.js
@@ -4,14 +4,9 @@ import { api } from "./AxiosService.js"
 
 
 class FavoriteService{
+
     async favorite(recipeId) {
         const userId = AppState.account.id
-        const favStatus = this.getFavoriteByRecipe(recipeId, userId)
-        if (favStatus){
-            const favoriteExist = new Favorite(favStatus)
-            const favoriteId = favoriteExist.id
-            this.trashFavorite(favoriteId)
-        }
         const response = await api.post("api/favorites", recipeId, userId)
         const favorite = new Favorite(response.data)
         AppState.favorites.push(favorite)
@@ -20,12 +15,6 @@ class FavoriteService{
     async trashFavorite(favoriteId) {
         const response = await api.get(`api/favorites/${favoriteId}`)
         return response
-    }
-
-    async getFavorites() {
-        const response = await api.get("api/favorites")
-        const favorites = response.data.map(favoriteData => new Favorite(favoriteData))
-        AppState.favorites = favorites        
     }
 
     async getFavoriteByRecipe(recipeId, userId){
