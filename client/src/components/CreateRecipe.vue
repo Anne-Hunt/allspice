786bcaccfda512c705a6700a683df292c50536de
Anne Hunt <acathunt@gmail.com>
2024-05-20 21:05:46 -0600

    updated forms and modal

---
+++ client/src/components/CreateRecipe.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+const image = ref({
+    link: ''
+})
+</script>
+
+
+<template>
+<div>
+<img :src="image.link" alt="">
+</div>
+<RecipeForm/>
+<IngredientsForm/>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -5,6 +5,7 @@ import { recipeService } from '../services/RecipeService.js';
 import { logger } from '../utils/Logger.js';
 import Pop from '../utils/Pop.js';
 import { accountService } from '../services/AccountService.js';
+import Navbar from '../components/Navbar.vue';
 
 
 const recipes = computed(()=>AppState.recipes)
@@ -44,11 +45,12 @@ onMounted(()=>
 
 <template>
 <div class="container-fluid">
-  <div class="row">
-    <div class="col-12 heroImg shadow"></div>
-    <div class="row justify-content-center">
-      <div class="col-6 shadow">
-        <div class="row p-3 text-success">
+  <div class="container-fluid heroImg shadow">
+    <Navbar/>
+    </div>
+  <div class="row justify-content-center mb-4">
+      <div class="col-6 shadow overlay">
+        <div class="row p-3 bg-light text-success align-items-end">
           <div class="col text-center" @click="filter()">Home</div>
           <div class="col text-center" @click="filter()">My Recipes</div>
           <div class="col text-center" @click="filter()">Favorites</div>
@@ -56,12 +58,12 @@ onMounted(()=>
       </div>
     </div>
   </div>
-  <div class="row g-3 mt-3">
-    <div class="col-md-3 col-2 d-flex" v-for="recipe in recipes" :key="recipe?.id">
+  <div class="row g-3 mt-5 px-3">
+    <div class="col-md-4 col-6 d-flex" v-for="recipe in recipes" :key="recipe?.id">
       <RecipeCard :recipe="recipe"/>
     </div>
   </div>
-</div>
+
 </template>
 
 <style scoped lang="scss">
@@ -69,5 +71,11 @@ onMounted(()=>
   background-image: url("https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
   background-size: cover;
   height: 50dvh;
+}
+
+.overlay{
+  position: absolute;
+  z-index: 1;
+  top: 45dvh;
 }
 </style>
