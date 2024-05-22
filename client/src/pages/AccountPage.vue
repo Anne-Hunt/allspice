70731a6f956c85ffc4f39a814d262820e8f0ad27
Anne Hunt <acathunt@gmail.com>
2024-05-22 11:34:51 -0600

    updated account page, updated recipe card for style

--- client/src/pages/AccountPage.vue
+++ client/src/pages/AccountPage.vue
@@ -8,6 +8,7 @@ import { accountService } from '../services/AccountService.js';
 
 const account = computed(() => AppState.account)
 const recipes = computed(()=> AppState.recipes.filter(recipe => recipe.creatorId == AppState.account.id))
+const favorites = computed(()=> AppState.recipeFans.filter(recipe => recipe.favorite.creatorId == AppState.account.id))
 
 async function editAccount(){
   try {
@@ -22,7 +23,10 @@ async function editAccount(){
 </script>
 
 <template>
-  <Navbar/>
+<div class="container-fluid m-0 p-0">
+  <div class="container-fluid heroImg shadow m-0 p-0">
+    <Navbar class="bg-dark"/>
+    </div>
   <div class="about text-center">
     <div v-if="account">
       <div class="text-end">
@@ -37,8 +41,14 @@ async function editAccount(){
     </div>
   </div>
   <div class="row g-3 mt-5 px-3">
+    <h4>Your Recipes</h4>
     <div class="col-md-4 col-6 d-flex" v-for="recipe in recipes" :key="recipe?.id">
       <RecipeCard :recipe="recipe"/>
+    </div>
+    <h4>Your Favorites</h4>
+    <div class="col-md-4 col-6 d-flex" v-for="favorite in favorites" :key="favorite?.id">
+      <RecipeCard :recipe="favorite"/>
+    </div>
     </div>
     </div>
 </template>
@@ -46,5 +56,11 @@ async function editAccount(){
 <style scoped lang="scss">
 img {
   max-width: 100px;
+}
+
+.heroImg{
+  background-image: url("https://plus.unsplash.com/premium_photo-1681401570418-4054ba349fa3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  background-size: cover;
+  height: 50dvh;
 }
 </style>
